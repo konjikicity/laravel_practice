f8e8c7f873a6e1aea1c149d795162cc91b11346a
syu.onoda <s-onoda@creative-innovation.co.jp>
2024-04-13 14:12:32 +0900

    fix: create method

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Requests\BookPostRequest;
 use App\Models\Author;
 use App\Models\Book;
+use Exception;
 use App\Models\Category;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\View\View;
@@ -46,16 +48,21 @@ class BookController extends Controller
 
     public function store(BookPostRequest $request): RedirectResponse
     {
-        DB::transaction(function () use ($request) {
-            $book = Book::create([
-                'category_id' => $request->category_id,
-                'title' => $request->title,
-                'price' => $request->price
-            ]);
+        try {
+            DB::transaction(function () use ($request) {
+                $book = Book::create([
+                    'category_id' => $request->category_id,
+                    'title' => $request->title,
+                    'price' => $request->price
+                ]);
 
-            $book->authors()->attach($request->author_ids);
-        });
+                $book->authors()->attach($request->author_ids);
+            });
 
-        return redirect(route('books.index'))->with('message', $request->title . 'を追加しました。');
+            return redirect(route('books.index'))->with('message', $request->title . 'を追加しました。');
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+            return redirect(route('books.index'))->with('message', 'エラーが発生しました。再度試してください。');
+        }
     }
 }
